1ed6a98977db91b037aed1b8517a42f857502ed9
Avainthefuture <37631@ocvts.org>
2025-01-21 16:35:28 +0000

    Final Part

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // These are variables that will be used in order to make the calculator.
+const description = {
+    name: "Calculator",
+    purpose: "Math", 
+    Use: "Unlimited",
+}
 let userInput = '';
 let userOperator = '';
 let num1 = 0; 
+let result = 0;
+let calculator = false;
+ let num3 = 0;
+ 
 
 // This will show how the calculator is going to work by displaying its elements. 
 const orderDisplay = document.getElementById('orderDisplay');
@@ -45,24 +54,72 @@ function calculate() {
 
     switch(userOperator) {
         case '+':
-        orderDisplay.innerHTML = add(num1, num2);
+        result = add(num1, num2);
+
         break; 
         case '-':
-        orderDisplay.innerHTML = subtract(num1, num2);
+        result = subtract(num1, num2);
         break;
       case '*':
-        orderDisplay.innerHTML = multiply(num1, num2);
+        result = multiply(num1, num2);
         break; 
         case '/':
-            orderDisplay.innerHTML = divide(num1, num2);
+            result = divide(num1, num2);
             break; 
             default:
-                orderDisplay.innerHTML = "Error, please add a symbol and another number, thank you.";
+                result = "Error, please add a symbol and another number, thank you.";
     }
+    // Helps with checking a right or wrong answer.
+    if (calculator == true) {
+        orderDisplay.innerHTML = result;
+    } else if (calculator == false) {
+        orderDisplay.innerHTML = "Please enter your answer";
+        userInput= '';
+        
+    }
+    
 }
 // Helps with clearing the calculator. 
 function clr() {
     orderDisplay.innerHTML = 0;
     userInput = '';
+    num1= 0;
+    num2 = 0; 
 }
+
+// Displays if the answer that the user put is right or not. 
+function correct() {
+     userInput = orderDisplay.innerHTML;
+     
+    if (userInput == result) {
+        orderDisplay.innerHTML = "Yes!";
+        } else {
+            orderDisplay.innerHTML = "No!";
+        }
+        
+}
+   
+// Object to describe what a calculator does. Help from W3Schools.com
+document.getElementById("description").innerHTML = description.name + ", " + description.purpose + ", " + description.Use;
+
+// Arrays for loop.
+const questions = ['When was the calculator invented?', 'Before electronic calculators, the ________ rule was a common tool for performing calculations.', 'True or False? The more advanced calculators are more expensive than basic calculators.']
+
+// Loop using the for method.
+for (let i = 0; i < questions.length; i++ ) {
+document.getElementById("q"+i).innerHTML = questions[i];
+
+}
+// Checks if an answer is correct.
+ function yes() {
+  document.getElementById('answer').innerHTML = 'That is correct!!';
+ }
+ 
+ // Checks if an answer is wrong.
+ function no() {
+document.getElementById('answer').innerHTML = 'That is wrong!! Please Try Again!!';
+ }
+// When was the calculator invented? Around 1642.
+// Before electronic calculators, the blank rule was a common tool for performing calculations. Slide.
+// True or False? The more advanced calculators are more expensive than basic calculators. True. 
 
